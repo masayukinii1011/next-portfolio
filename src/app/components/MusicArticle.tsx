36509218c2612e696b46975db27496dad7dd6b2d
masayukinii1011 <masayukinii1011@gmail.com>
2024-12-13 20:27:05 +0900

    lazy

--- src/app/components/MusicArticle.tsx
+++ src/app/components/MusicArticle.tsx
@@ -11,6 +11,7 @@ export default function MusicArticle() {
 				className="mb-8 max-w-full"
 				src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1092547429&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
 				title="SoundCloud Playlist 1"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -19,6 +20,7 @@ export default function MusicArticle() {
 				className="mb-8 max-w-full"
 				src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/1486647112&color=%23ff5500&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true"
 				title="SoundCloud Playlist 2"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -29,6 +31,7 @@ export default function MusicArticle() {
 				sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
 				src="https://embed.music.apple.com/jp/album/matters-of-the-affection/1536910723"
 				title="Apple Music Album 1"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -39,6 +42,7 @@ export default function MusicArticle() {
 				sandbox="allow-forms allow-popups allow-same-origin allow-scripts allow-storage-access-by-user-activation allow-top-navigation-by-user-activation"
 				src="https://embed.music.apple.com/jp/album/underneath-the-city-of-the-bloom/1334493546"
 				title="Apple Music Album 2"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -49,6 +53,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 1"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -59,6 +64,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 2"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -69,6 +75,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 3"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -79,6 +86,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 4"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -89,6 +97,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 5"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -99,6 +108,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 6"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -109,6 +119,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 7"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
@@ -119,6 +130,7 @@ export default function MusicArticle() {
 				allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
 				allowFullScreen
 				title="YouTube Video 8"
+				loading="lazy"
 				width="640"
 				height="360"
 			/>
